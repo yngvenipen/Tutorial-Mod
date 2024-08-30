38a3c09febfbf53b57c58fc46496ef8885a18072
Yngve Midtun Nipen <yngve@nipen.no>
2024-08-30 19:39:54 +0200

    Added Custom Blocks!

--- src/main/java/net/yngve/tutorialmod/TutorialMod.java
+++ src/main/java/net/yngve/tutorialmod/TutorialMod.java
@@ -2,15 +2,18 @@ package net.yngve.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.yngve.tutorialmod.block.ModBlocks;
+import net.yngve.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
