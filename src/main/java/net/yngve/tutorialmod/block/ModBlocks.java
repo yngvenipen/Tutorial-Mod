68fbd592267fb7d709e3f033f7d6e603a1e648f5
Yngve Midtun Nipen <yngve@nipen.no>
2025-02-23 19:01:31 +0100

    Added custom recipes and loot tables!

--- src/main/java/net/yngve/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/yngve/tutorialmod/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.yngve.tutorialmod.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.yngve.tutorialmod.TutorialMod;
 
 public class ModBlocks {
@@ -27,6 +29,15 @@ public class ModBlocks {
                     .requiresTool()
             )
     );
+
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool()));
+
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
 
 
     private static Block registerBlock(String name, Block block) {
